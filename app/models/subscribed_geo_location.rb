26a9e1d427ffb5f8b2cbcd7725336749c95cd0d5
Brad Langhorst <langhorst@neb.com>
2021-02-24 07:40:17 -0500

    rubocop fixes

--- app/models/subscribed_geo_location.rb
+++ app/models/subscribed_geo_location.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 class SubscribedGeoLocation < ApplicationRecord
   belongs_to :user
   belongs_to :geo_location
 
   validates_uniqueness_of :user_id, scope: :geo_location_id
-
 end
