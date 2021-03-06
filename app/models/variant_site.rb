64eaec2c76defbf504f978f66b07428658919d12
Brad Langhorst <langhorst@neb.com>
2021-03-06 18:54:07 -0500

    avoids one more db lookup loop

--- app/models/variant_site.rb
+++ app/models/variant_site.rb
@@ -10,8 +10,8 @@ class VariantSite < ApplicationRecord
     File.readlines(variants_tsv).each do |line|
       parsed_fields = line.chomp.split("\t")
       (strain, ref_pos, variant_type, variant) = parsed_fields
-      fasta_record = FastaRecord.find_by(strain: strain)
-      fasta_record_id = fasta_record.id
+      fasta_record_id = FastaRecord.existing_fasta_strain_ids[strain]
+      raise "Failed to find fasta record for stain #{strain}" unless fasta_record_id
 
       next unless strain && ref_pos && variant_type && variant
 
