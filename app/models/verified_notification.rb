9638ead50f5f24db9d5e9966808cbe00b8c1a4c4
Brad Langhorst <langhorst@neb.com>
2021-03-07 08:41:53 -0500

    rubocop fixes

--- app/models/verified_notification.rb
+++ app/models/verified_notification.rb
@@ -1,30 +1,28 @@
 # frozen_string_literal: true
 
 class VerifiedNotification < ApplicationRecord
-    belongs_to :users
-    has_many :proposed_notifications
+  belongs_to :users
+  has_many :proposed_notifications
 
-    def self.group_notifications()
+  def self.group_notifications
+    # I want to find proposed notifications without a verified notification id, group by user, then
+    # add a new verified record and put the new verified_record_id in the proposed notifications table for those
+    # records
+    # It seems to work, but there must be a better way of doing this
+    new_notifications = ProposedNotification.where(verified_notification_id: nil)
+                                            .select(:user_id)
+                                            .group(:user_id)
 
-        # I want to find proposed notifications without a verified notification id, group by user, then
-        # add a new verified record and put the new verified_record_id in the proposed notifications table for those
-        # records
-        # It seems to work, but there must be a better way of doing this
-        new_notifications = ProposedNotification.where(:verified_notification_id => nil)
-                                                .select(:user_id)
-                                                .group(:user_id)
-                                                
-        new_notifications.each do |notification|
+    new_notifications.each do |notification|
+      unless VerifiedNotification.exists?(user_id: notification.user_id, status: 'Unsent')
+        VerifiedNotification.new(user_id: notification.user_id, status: 'Unsent').save!
+      end
 
-            unless VerifiedNotification.exists?(user_id: notification.user_id, status: 'Unsent')
-                VerifiedNotification.new(user_id: notification.user_id, status: 'Unsent').save!
-            end
+      verified_id = VerifiedNotification.find_by(user_id: notification.user_id, status: 'Unsent').id
 
-            verified_id = VerifiedNotification.find_by(user_id: notification.user_id, status: 'Unsent').id
-
-            ProposedNotification.where(:user_id => notification.user_id)
-                                .where(:verified_notification_id => nil)
-                                .update_all(:verified_notification_id => verified_id)
-        end
+      ProposedNotification.where(user_id: notification.user_id)
+                          .where(verified_notification_id: nil)
+                          .update_all(verified_notification_id: verified_id)
     end
+  end
 end
