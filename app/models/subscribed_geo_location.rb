57cac99e6d7e0bd2602cff97b1549ee6f9f7ad84
Brad Langhorst <langhorst@neb.com>
2021-04-16 19:58:34 -0400

    shows subscriptions on user page

--- app/models/subscribed_geo_location.rb
+++ app/models/subscribed_geo_location.rb
@@ -5,4 +5,8 @@ class SubscribedGeoLocation < ApplicationRecord
   belongs_to :detailed_geo_location_alias
 
   validates_uniqueness_of :user_id, scope: :detailed_geo_location_alias_id
+
+  def to_s
+    detailed_geo_location_alias.name
+  end
 end
