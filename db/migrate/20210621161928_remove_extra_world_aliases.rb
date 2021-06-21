7ec18fe2fd539cc8442bfed2fd49533612a81a24
Brad Langhorst <langhorst@neb.com>
2021-06-21 12:37:03 -0400

    missing end...

--- db/migrate/20210621161928_remove_extra_world_aliases.rb
+++ db/migrate/20210621161928_remove_extra_world_aliases.rb
@@ -6,4 +6,5 @@ class RemoveExtraWorldAliases < ActiveRecord::Migration[6.1]
     where exists (select 1 from detailed_geo_location_aliases dgla2 where dgla2.id < dgla1.id and dgla1.world = dgla2.world
     and dgla1.region is null and dgla2.region is null);
     SQL
+  end
 end
