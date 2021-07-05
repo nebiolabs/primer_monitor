08246f388e50fb40356c7ef09891d5047529e23f
Brad Langhorst <langhorst@neb.com>
2021-07-05 06:48:37 -0400

    proper order syntax

--- upload.rb
+++ upload.rb
@@ -78,8 +78,8 @@ def send_notifications(verified_notifications)
     pns = ProposedNotification.where(verified_notification_id: vn.id)
                               .includes(:oligo, :primer_set,
                                         :subscribed_geo_location, :detailed_geo_location_alias)
-                              .order(primer_set: :name, oligo: %i[locus category],
-                                     detailed_geo_location_alias: %i[region subregion division subdivision])
+                              .reorder('primer_sets.name, oligos.locus, oligos.category,' +
+                                         'region, subregion, division, subdivision')
     PrimerSetMailer.primer_overlap_notification_email(vn.user_id, pns).deliver_now
     vn.status = 'Sent'
     vn.save!
