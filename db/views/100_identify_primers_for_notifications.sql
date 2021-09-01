9c18fcee9da806c09bf565916b242a8a57286ffd
Brad Langhorst <langhorst@neb.com>
2021-09-01 10:26:13 -0400

    clearer names, maybe working?

--- db/views/100_identify_primers_for_notifications.sql
+++ db/views/100_identify_primers_for_notifications.sql
@@ -25,8 +25,10 @@ CREATE MATERIALIZED VIEW public.identify_primers_for_notifications AS
              JOIN public.users ON ((users.id = primer_set_subscriptions.user_id)))
           WHERE (oligo_variant_overlaps.date_collected >= (CURRENT_DATE - users.lookback_days))
           AND primer_set_subscriptions.active = true
+          --and users.id =20
           GROUP BY primer_set_subscriptions.user_id, primer_set_subscriptions.primer_set_id, primer_sets.name, oligos.id, oligos.name, join_subscribed_location_to_ids.detailed_geo_location_id, users.lookback_days, users.variant_fraction_threshold, oligo_variant_overlaps.region, oligo_variant_overlaps.subregion, oligo_variant_overlaps.division, oligo_variant_overlaps.subdivision, oligo_variant_overlaps.coords, oligo_variant_overlaps.detailed_geo_location_id
-        ), second_query AS (
+        ),
+      total_sequences_for_denominator AS (
          SELECT fasta_records.detailed_geo_location_id,
             count(fasta_records.id) AS records_count,
             users.lookback_days
@@ -34,6 +36,7 @@ CREATE MATERIALIZED VIEW public.identify_primers_for_notifications AS
              JOIN public.join_subscribed_location_to_ids ON ((join_subscribed_location_to_ids.detailed_geo_location_id = fasta_records.detailed_geo_location_id)))
              JOIN public.users ON ((join_subscribed_location_to_ids.user_id = users.id)))
           WHERE (fasta_records.date_collected >= (CURRENT_DATE - users.lookback_days))
+         --and users.id = 20
           GROUP BY fasta_records.detailed_geo_location_id, users.lookback_days
          HAVING (count(fasta_records.id) >= 20)
         )
@@ -48,13 +51,18 @@ CREATE MATERIALIZED VIEW public.identify_primers_for_notifications AS
     first_query.subdivision,
     first_query.coords,
     first_query.variant_count,
+    total_sequences_for_denominator.records_count,
     first_query.variant_fraction_threshold,
-    second_query.detailed_geo_location_id,
-    second_query.records_count,
-    ((first_query.variant_count)::numeric / (second_query.records_count)::numeric) AS fraction_variant
+    total_sequences_for_denominator.detailed_geo_location_id,
+    ((first_query.variant_count)::numeric / (total_sequences_for_denominator.records_count)::numeric) AS fraction_variant
    FROM (first_query
-     JOIN second_query ON ((second_query.detailed_geo_location_id = first_query.detailed_geo_location_id)))
-  WHERE ((((first_query.variant_count)::numeric / (second_query.records_count)::numeric))::double precision >= first_query.variant_fraction_threshold)
-  ;
+     JOIN total_sequences_for_denominator ON (
+         total_sequences_for_denominator.detailed_geo_location_id = first_query.detailed_geo_location_id
+         AND
+         total_sequences_for_denominator.lookback_days = first_query.lookback_days
+         )
+     )
+  WHERE ((((first_query.variant_count)::numeric / (total_sequences_for_denominator.records_count)::numeric))::double precision >= first_query.variant_fraction_threshold)
+;
 
 GRANT SELECT on identify_primers_for_notifications to primer_monitor_ro;
