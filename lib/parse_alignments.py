91ed2210646970110c8afc5df1a3da5a85a47f93
Brad Langhorst <langhorst@neb.com>
2022-01-26 23:21:43 -0500

    fixes a problem with CIGAR parsing that caused shifted positions of variants adds a test for correct output from parser

--- lib/parse_alignments.py
+++ lib/parse_alignments.py
@@ -19,19 +19,19 @@ for line in sys.stdin:
 
     # print(parsed_cigar)
     query_start = 0
+    read_consuming_ops = ("M", "I", "S", "=", "X", "H")
+    ref_consuming_ops = ("M", "D", "I", "N", "=", "X")
     for item in parsed_cigar:
         length = int(item[:-1])
         type = item[len(item)-1:]
-
-        if type == "S":
-            None # Don't do anything, but still add the length to query_start below
-        elif type != "=":
-            if type == "I":
-                print("{}\t{}\t{}\t{}".format(query_name, ref_start, type, query_seq[query_start : query_start + 1]))
-            else:
-                print("{}\t{}\t{}\t{}".format(query_name, ref_start, type, query_seq[query_start : query_start + length]))
-                
-
-        query_start += length
-        ref_start += length
+        if type == "D":
+            output = "-" * length
+        else:
+            output = query_seq[query_start : query_start + length]
+        if not type in "MHS=": #dont print match or clips
+            print(f"{query_name}\t{ref_start}\t{type}\t{output}")
+        if type in read_consuming_ops:
+            query_start += length
+        if type in ref_consuming_ops:
+            ref_start += length
         
