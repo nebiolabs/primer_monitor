a2bcd31c397939e39fd0dff16ff546916d67a56c
Caiden <ckumar@neb.com>
2022-11-17 13:39:01 -0500

    Cleaned up filter_duplicates.py

--- lib/filter_duplicates.py
+++ lib/filter_duplicates.py
@@ -4,18 +4,20 @@ import re
 
 # Usage: ./filter_duplicates.py <old data> <new data> > <output>
 
-old_set = set([])
-old_records = open(sys.argv[1], "r")
-for line in old_records:
-    regex = re.search('\"covv_virus_name\": \"(.*?)\",', line)
-    strain = regex.group(1)
-    old_set.add(strain)
-old_records.close()
+def get_strain(line):
+        regex = re.search('\"covv_virus_name\": \"(.*?)\",', line)
+        return regex.group(1)
 
-new_records = open(sys.argv[2], "r")
-for line in new_records:
-    regex = re.search('\"covv_virus_name\": \"(.*?)\",', line)
-    strain = regex.group(1)
-    if strain not in old_set:
-        print(line.strip())
-old_records.close()
+old_set = set([])
+with open(sys.argv[1], "r") as old_records:
+    for line in old_records:
+        if line.strip() != "": # skip blank lines
+            strain = get_strain(line)
+            old_set.add(strain)
+
+with open(sys.argv[2], "r") as new_records:
+    for line in new_records:
+        if line.strip() != "": # skip blank lines
+            strain = get_strain(line)
+            if strain not in old_set:
+                print(line.strip())
