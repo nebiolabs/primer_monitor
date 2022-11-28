189b348f78d26769b09d7dadb0b8d83d88ac39f4
Caiden <ckumar@neb.com>
2022-11-28 10:59:57 -0500

    Made filter_duplicates handle same-day dups.

--- lib/filter_duplicates.py
+++ lib/filter_duplicates.py
@@ -8,17 +8,18 @@ def get_strain(line):
     regex = re.search('\"covv_virus_name\": \"(.*?)\",', line)
     return regex.group(1)
 
-old_set = set([])
+record_set = set([])
 with open(sys.argv[1], "r") as old_records:
     for line in old_records:
         if line.strip() != "": # skip blank lines
             strain = get_strain(line)
-            old_set.add(strain)
+            record_set.add(strain)
 
 with open(sys.argv[2], "r") as new_records:
     for line in new_records:
         line_stripped = line.strip() # so line.strip() isn't called twice
         if line_stripped != "": # skip blank lines
             strain = get_strain(line)
-            if strain not in old_set:
+            if strain not in record_set:
                 print(line_stripped)
+                record_set.add(strain) # do not output two of the same record, old or new
