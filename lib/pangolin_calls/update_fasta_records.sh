7f21855a4402387743cb9134b7b53453d7d2f745
Caiden <ckumar@neb.com>
2023-01-04 16:46:17 -0500

    Shellcheck.

--- lib/pangolin_calls/update_fasta_records.sh
+++ lib/pangolin_calls/update_fasta_records.sh
@@ -3,5 +3,5 @@ if (($# < 1)); then
     exit 1;
 fi
 
-psql -h $DB_HOST -d $DB_NAME -U $DB_USER -f update_fasta_records.sql -v pangolin_csv_path=\'$1\';
-touch $1.complete;
+psql -h "$DB_HOST" -d "$DB_NAME" -U "$DB_USER" -f update_fasta_records.sql -v pangolin_csv_path=\'"$1"\';
+touch "$1".complete;
