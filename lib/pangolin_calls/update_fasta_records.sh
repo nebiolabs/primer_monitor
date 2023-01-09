8e94962babb2854b09f7164cc83c68e3b9a11ac8
Caiden <ckumar@neb.com>
2023-01-09 17:18:27 -0500

    Fixed non-working heredoc and bugged SQL.

--- lib/pangolin_calls/update_fasta_records.sh
+++ lib/pangolin_calls/update_fasta_records.sh
@@ -5,5 +5,11 @@ if (($# < 1)); then
     exit 1;
 fi
 
-psql -d primer_monitor_dev -f update_fasta_records.sql -v pangolin_csv_path=\'"$1"\';
+psql -d primer_monitor_dev >&2 <<CMDS;
+CREATE TEMPORARY TABLE pangolin_tmp (taxon varchar NOT NULL, lineage varchar NOT NULL, conflict varchar, ambiguity_score float4, scorpio_call varchar, scorpio_support numeric, scorpio_conflict numeric, scorpio_notes text, version varchar NOT NULL, pangolin_version date NOT NULL, scorpio_version varchar NOT NULL, constellation_version varchar NOT NULL, is_designated text NOT NULL, qc_status varchar, qc_notes varchar, note varchar);
+\copy pangolin_tmp from '$1' WITH (format csv, header on);
+UPDATE fasta_records SET pangolin_lineage=pangolin_tmp.lineage FROM pangolin_tmp WHERE pangolin_tmp.taxon=fasta_records.strain;
+DROP TABLE pangolin_tmp;
+CMDS
+
 touch "$1.$(basename "$PWD").complete_pangolin";
