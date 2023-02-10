49031161f29d5888be2be0a9d4bc6c646c5d70c3
Caiden <ckumar@neb.com>
2023-02-10 10:32:49 -0500

    Added default of 180 days.

--- lib/visualization/process_primer_sets_with_lineages.sh
+++ lib/visualization/process_primer_sets_with_lineages.sh
@@ -8,6 +8,10 @@ threads="$5";
 primer_sets_list_path="$6";
 buffer_size="$7";
 
+if [[ $cutoff_date == '-' ]]; then # default of 180 days
+  cutoff_date="$(date -d "180 days ago" +"%Y-%m-%d")"
+fi
+
 variants_bed="$$_variants.bed"
 variants_counts_bed="$$_variants_with_counts.bed";
 
