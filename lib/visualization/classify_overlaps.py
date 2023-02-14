74437aaa619133d129c588f39bf274de329db1c0
Caiden <ckumar@neb.com>
2023-02-14 17:46:58 -0500

    Updated scoring scheme.

--- lib/visualization/classify_overlaps.py
+++ lib/visualization/classify_overlaps.py
@@ -5,6 +5,8 @@ NEAR_3P_SCORE = 2
 MID_3P_SCORE = 4
 OTHER_SCORE = 3
 CUTOFF = int(sys.argv[3])
+DELETION_MULTIPLIER = 10
+LENGTH_EXPONENT_BASE = 2
 
 primers_scored = {}
 
@@ -12,6 +14,7 @@ with open(sys.argv[1]) as f:
     for line_s in f:
         line = line_s.strip().split("\t")
         variant_start_ind = len(line)-1
+        variant_name = None
         while line[variant_start_ind] != line[0]:
             variant_start_ind-=1
         try:
@@ -19,9 +22,11 @@ with open(sys.argv[1]) as f:
             line[2] = int(line[2])
             line[variant_start_ind+1] = int(line[variant_start_ind+1])
             line[variant_start_ind+2] = int(line[variant_start_ind+2])
+            variant_name = line[variant_start_ind+3]
         except:
             print(line)
             raise
+        cigar_op = variant_name.split("/")[0]
         overlap_start = (line[1] if line[1] > line[variant_start_ind+1] else line[variant_start_ind+1])
         overlap_end = (line[2] if line[variant_start_ind+2] < line[variant_start_ind+2] else line[variant_start_ind+2])
         overlap_len = overlap_end-overlap_start
@@ -30,25 +35,34 @@ with open(sys.argv[1]) as f:
         fivep_dist = None
         threep_dist = None
         dir = (1 if line[5] == "+" else -1)
+        new_score = 0
         if dir > 0:
             fivep_dist = overlap_start-line[1]
             threep_dist = line[2]-overlap_start
         else:
             fivep_dist = line[2]-overlap_start
             threep_dist = overlap_start-line[1]
+
+        if cigar_op == "I" and overlap_start<int(line[1]): # insertion outside primer
+            continue
+
         while pos <= overlap_end:
             if fivep_dist <= 3:
-                score+=NEAR_5P_SCORE
+                new_score = NEAR_5P_SCORE
             elif threep_dist <= 2:
-                score+=NEAR_3P_SCORE
+                new_score = NEAR_3P_SCORE
             elif threep_dist >2 and threep_dist <= 5:
-                score+=MID_3P_SCORE
+                new_score = MID_3P_SCORE
             else:
-                score+=OTHER_SCORE
+                new_score = OTHER_SCORE
 
             fivep_dist+=dir
             threep_dist-=dir
             pos+=1
+            if cigar_op == "D": # deletion
+                new_score = new_score * DELETION_MULTIPLIER
+
+            score += new_score
 
         if line[3] not in primers_scored:
             primers_scored[line[3]]=line[:6]
@@ -59,10 +73,10 @@ with open(sys.argv[1]) as f:
     affected = open(sys.argv[2], "w")
     for primer in primers_scored:
         score = primers_scored[primer][4]
-        if score > CUTOFF:
+        if (LENGTH_EXPONENT_BASE**score) > CUTOFF:
              i = 0
              for field in primers_scored[primer]:
                 primers_scored[primer][i] = str(primers_scored[primer][i])
                 i+=1
-            affected.write("\t".join(primers_scored[primer])+"\n")
+             affected.write("\t".join(primers_scored[primer])+"\n")
     affected.close()
