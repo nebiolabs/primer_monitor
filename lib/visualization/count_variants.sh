bc296cc8e5f218083f04271e47f43ee2b259f9c3
Caiden <ckumar@neb.com>
2023-02-14 13:30:20 -0500

    Fixed script to use percent of sequences not percent of variants.

--- lib/visualization/count_variants.sh
+++ lib/visualization/count_variants.sh
@@ -5,17 +5,17 @@ min_pct="$2";
 threads="$3";
 lineage_set="$4";
 
-./filter_variants.awk "$lineage_set" < "$variants_bed" > "$$_filtered_variants.bed"
+./filter_variants.awk "$lineage_set" < "$variants_bed" "$$_strains.txt" > "$$_filtered_variants.bed"
 
-variant_count=$(wc -l < "$$_filtered_variants.bed" | sed -E "s/^\s*//"); #get just the line count and no leading spaces
+sequence_count=$(wc -l "$$_strains.txt" | sed -E "s/^\s*//" | cut -f 1 -d" "); #get just the line count and no leading spaces or file name
 
-# this basically does int(variant_count*(min_pct/100)) to use min_pct as a percent
+# this basically does int(sequence_count*(min_pct/100)) to use min_pct as a percent
 scaled_min_count=$(bc <<CALC
-scale=10; v=$variant_count; p=$min_pct/100; s=v*p; scale=0; s=s/1; s;
+scale=10; v=$sequence_count; p=$min_pct/100; s=v*p; scale=0; s=s/1; s;
 CALC
 )
 
-#echo "$(date +'%b %d %H:%M:%S') - variant count is $variant_count, cutoff is $scaled_min_count" >&2
+#echo "$(date +'%b %d %H:%M:%S') - sequence count is $sequence_count, cutoff is $scaled_min_count" >&2
 
 if ((scaled_min_count < 10)); then
   scaled_min_count=10; # exclude variants that occur <10 times in all cases
--- lib/visualization/filter_variants.awk
+++ lib/visualization/filter_variants.awk
@@ -6,6 +6,8 @@ BEGIN {
         all = 1
         ARGV[1] = "" # skip this "file"
     }
+
+    strainsfile = ARGV[2]
     ARGV[2] = "-" # read data from stdin
     ARGC = 3
 }
@@ -18,5 +20,13 @@ FILENAME == ARGV[2] { # BED records, FILENAME is "-"
     if ($5 in lineages || all == 1) # either in lineage list or set to "all"
     {
         print
+        strains[$4] = 1 # record strain seen
+    }
+}
+
+END {
+    for (strain in strains)
+    {
+        print strain >> strainsfile
     }
 }
