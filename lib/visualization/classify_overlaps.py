f82bc7dc6e6c9be4fb4ebe288e9ff152fc7e5b27
Caiden <ckumar@neb.com>
2023-02-21 13:51:49 -0500

    Cleaned up code.

--- lib/visualization/classify_overlaps.py
+++ lib/visualization/classify_overlaps.py
@@ -14,40 +14,48 @@ primers_scored = {}
 with open(sys.argv[1]) as f:
     for line_s in f:
         line = line_s.strip().split("\t")
+
         variant_start_ind = len(line)-1
+        primer_start_pos = int(line[1])
+        primer_end_pos = int(line[2])
+        strand = line[5]
+        seq_name = line[3]
+
+        variant_start_pos = None
+        variant_end_pos = None
         variant_name = None
+
         while line[variant_start_ind] != line[0]:
             variant_start_ind-=1
         try:
-            line[1] = int(line[1])
-            line[2] = int(line[2])
-            line[variant_start_ind+1] = int(line[variant_start_ind+1])
-            line[variant_start_ind+2] = int(line[variant_start_ind+2])
+            variant_start_pos = int(line[variant_start_ind+1])
+            variant_end_pos = int(line[variant_start_ind+2])
             variant_name = line[variant_start_ind+3]
         except:
             print(line)
             raise
+
         cigar_op = variant_name.split("/")[0]
-        overlap_start = (line[1] if line[1] > line[variant_start_ind+1] else line[variant_start_ind+1])
-        overlap_end = (line[2] if line[variant_start_ind+2] < line[variant_start_ind+2] else line[variant_start_ind+2])
+        overlap_start = (primer_start_pos if primer_start_pos > variant_start_pos else variant_start_pos)
+        overlap_end = (primer_end_pos if primer_end_pos < variant_end_pos else variant_end_pos)
         overlap_len = overlap_end-overlap_start
         score = 0
         pos = overlap_start
         dist_5p = None
         dist_3p = None
-        dir = (1 if line[5] == "+" else -1)
-        new_score = 0
+        dir = (1 if strand == "+" else -1)
         if dir > 0:
-            dist_5p = overlap_start-line[1]
-            dist_3p = line[2]-overlap_start
+            dist_5p = overlap_start-primer_start_pos
+            dist_3p = primer_end_pos-overlap_start
         else:
-            dist_5p = line[2]-overlap_start
-            dist_3p = overlap_start-line[1]
+            dist_5p = primer_end_pos-overlap_start
+            dist_3p = overlap_start-primer_start_pos
 
-        if cigar_op == "I" and overlap_start<int(line[1]): # insertion outside primer
+        if cigar_op == "I" and overlap_start<primer_start_pos: # insertion outside primer
             continue
 
         while pos <= overlap_end:
+            new_score = 0
             if dist_5p <= 3:
                 new_score = NEAR_5P_SCORE
             elif dist_3p <= 2:
@@ -65,23 +73,19 @@ with open(sys.argv[1]) as f:
 
             score += new_score
 
-        if line[3] not in primers_scored:
-            primers_scored[line[3]]=line[:6]
-            primers_scored[line[3]][4] = 0
+        if seq_name not in primers_scored:
+            primers_scored[seq_name]=line[:6] # get BED record for primer
+            primers_scored[seq_name][4] = 0 # init score
 
-        primers_scored[line[3]][4]+=(LENGTH_EXPONENT_BASE**score)
+        primers_scored[seq_name][4]+=(LENGTH_EXPONENT_BASE**score) # update score
 
     affected = open(sys.argv[2], "w")
     for primer in primers_scored:
-        score = primers_scored[primer][4]
+        score = int(primers_scored[primer][4])
         if score > CUTOFF:
-            primers_scored[primer][4] = math.log(primers_scored[primer][4]) # ln to reduce the range of scores
-            if primers_scored[primer][4] > 1000:
+            score = math.log(score) # ln to reduce the range of scores
+            if score > 1000:
                 score = 1000 # cap at 1000 to follow BED spec
-            i = 0
-            for field in primers_scored[primer]: # convert non-string fields to strings before .join("\t")
-                if type(primers_scored[primer][i]) != type("a string"):
-                    primers_scored[primer][i] = str(primers_scored[primer][i])
-                i+=1
+            primers_scored[primer][4] = str(score)
             affected.write("\t".join(primers_scored[primer])+"\n")
     affected.close()
