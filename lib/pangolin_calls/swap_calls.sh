a0d50faece08a0c41bec4729c3c3b252c23d8df9
Caiden <ckumar@neb.com>
2023-06-09 16:28:45 -0400

    Fixed swap_calls wanting an extra argument.

--- lib/pangolin_calls/swap_calls.sh
+++ lib/pangolin_calls/swap_calls.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
 if (($# < 1)); then
-    echo "usage: ./swap_calls.sh [pending_only]" >&2;
-    exit 1;
-fi
-
-if (($# < 2)); then
     pending="";
 else
-    pending=$2;
+    pending=$1;
 fi
 
 source ../../.env;
@@ -17,4 +12,4 @@ psql -h "$DB_HOST" -d "$DB_NAME" -U "$DB_USER" >&2 <<CMDS;
 UPDATE fasta_records SET fasta_records.pangolin_call_id=fasta_records.pending_pangolin_call_id, fasta_records.pending_pangolin_call_id=NULL, WHERE fasta_records.pending_pangolin_call_id IS NOT NULL AND fasta_records.pangolin_call_id IS $pending NULL;
 CMDS
 
-touch "$1.$(basename "$PWD").complete_swap";
+touch "$(basename "$PWD").complete_swap";
