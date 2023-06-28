4699762953efb890c1d923b229d36cef7a5f3ef9
Caiden <ckumar@neb.com>
2023-06-28 13:16:09 -0400

    Fixed GISAID leftover.

--- lib/visualization/extract_all_variants.sh
+++ lib/visualization/extract_all_variants.sh
@@ -9,7 +9,7 @@ fi
 # WARNING: do not allow arbitrary user data for date cutoff and lineage set, SQL injection risk
 
 psql -h "$DB_HOST" -d "$DB_NAME" -U "$DB_USER" -c "SELECT variant_sites.ref_start, variant_sites.ref_end, \
-fasta_records.strain, fasta_records.pangolin_lineage, fasta_records.date_collected, variant_sites.variant_type, \
+fasta_records.genbank_accession, fasta_records.pangolin_lineage, fasta_records.date_collected, variant_sites.variant_type, \
 variant_sites.variant FROM variant_sites INNER JOIN fasta_records ON fasta_records.id=variant_sites.fasta_record_id \
 WHERE (date_collected >= '$1' OR (date_collected IS NULL AND date_submitted >= '$1')) \
 ORDER BY variant_sites.ref_start, variant_sites.ref_end, variant_sites.variant_type, variant_sites.variant;" --csv -t \
