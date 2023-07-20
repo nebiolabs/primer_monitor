ececf691e782a0c84261c62752ec738750bc7631
Caiden <ckumar@neb.com>
2023-07-20 17:51:37 -0400

    Made extract_all_variants.sh not hardcode accession number.

--- lib/visualization/extract_all_variants.sh
+++ lib/visualization/extract_all_variants.sh
@@ -12,13 +12,14 @@ fi
 
 # WARNING: do not allow arbitrary user data for date cutoff and lineage set, SQL injection risk
 
-psql -h "$DB_HOST" -d "$DB_NAME" -U "$DB_USER_RO" -c "SELECT variant_sites.ref_start, variant_sites.ref_end, \
+psql -h "$DB_HOST" -d "$DB_NAME" -U "$DB_USER_RO" -c "SELECT organisms.reference_accession, variant_sites.ref_start, variant_sites.ref_end, \
 fasta_records.genbank_accession, lineages.name, fasta_records.date_collected, variant_sites.variant_type, \
 variant_sites.variant FROM variant_sites \
 INNER JOIN fasta_records ON fasta_records.id=variant_sites.fasta_record_id \
 INNER JOIN pangolin_calls ON fasta_records.pangolin_call_id=pangolin_calls.id \
 INNER JOIN lineages ON pangolin_calls.lineage_id=lineages.id \
+INNER JOIN organisms ON organisms.id=lineages.organism_id \
 WHERE (date_collected >= '$1
 ' OR (date_collected IS NULL AND date_submitted >= '$1')) \
 ORDER BY variant_sites.ref_start, variant_sites.ref_end, variant_sites.variant_type, variant_sites.variant;" --csv -t \
-| tr "," "\t" | sed -E "s/^/NC_045512.2\t/g" | grep -v -E "\t[0-9]+N"
+| tr "," "\t" | grep -v -E "\t[0-9]+N"
