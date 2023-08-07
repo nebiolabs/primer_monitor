dd5803eea2ddceb76b7b2c380a7c9a9942c33ddd
Caiden <ckumar@neb.com>
2023-08-07 11:09:41 -0400

    Added composite_primary_key.

--- app/models/identify_primers_for_notification.rb
+++ app/models/identify_primers_for_notification.rb
@@ -5,6 +5,8 @@ class IdentifyPrimersForNotification < ApplicationRecord
   belongs_to :primer_set
   belongs_to :oligo
 
+  self.primary_keys = :primer_set_id, :user_id, :oligo_id, :coords, :detailed_geo_location_id
+
   def coordinate
     coords
   end
