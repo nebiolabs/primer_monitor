c211518ab6a2dac4c54d4430f91f197ce97d9cbc
Caiden <ckumar@neb.com>
2023-08-15 17:40:21 -0400

    Adding cron job scripts to repo.

---
+++ lib/backend_scripts/primer-monitor.sh
@@ -0,0 +1,14 @@
+source "$(dirname "$0")/../../.env";
+mkdir -p "$BACKEND_SCRATCH_PATH/status";
+
+"$NEXTFLOW_INSTALL_PATH" -log "$BACKEND_SCRATCH_PATH/log_download/" \
+run "$BACKEND_INSTALL_PATH/lib/summarize_variants.nf" \
+--ref "$BACKEND_INSTALL_PATH/igvstatic/2697049/ref/NC_045512.2.fasta" \
+-w "$BACKEND_SCRATCH_PATH/work_download/" \
+--pangolin_version_path "$BACKEND_INSTALL_PATH/pangolin_ver.txt" \
+--pangolin_data_version_path "$BACKEND_INSTALL_PATH/pangolin_data_ver.txt" \
+--flag_path "$BACKEND_SCRATCH_PATH/status" \
+--igvstatic_path "$FRONTEND_HOST" \
+--frontend_host "$FRONTEND_HOST" \
+--jump_proxy "$JUMP_PROXY" \
+-N "$NOTIFICATION_EMAILS";
---
+++ lib/backend_scripts/recall-pangolin.sh
@@ -0,0 +1,11 @@
+source "$(dirname "$0")/../../.env";
+mkdir -p "$BACKEND_SCRATCH_PATH/status";
+
+"$NEXTFLOW_INSTALL_PATH" -log "$BACKEND_SCRATCH_PATH/log_pangolin/" \
+run "$BACKEND_INSTALL_PATH/lib/pangolin_calls/recall_pangolin.nf" \
+-w "$BACKEND_SCRATCH_PATH/work_pangolin/" \
+--pangolin_version_path "$BACKEND_INSTALL_PATH/pangolin_ver.txt" \
+--pangolin_data_version_path "$BACKEND_INSTALL_PATH/pangolin_data_ver.txt" \
+--flag_path "$BACKEND_SCRATCH_PATH/status" \
+-N "$NOTIFICATION_EMAILS" \
+-c "$BACKEND_INSTALL_PATH/lib/nextflow.config"
