87ef56d74b87c6a95428d6d083bbf2469f400ecf
Caiden <ckumar@neb.com>
2023-08-22 13:59:48 -0400

    Removed compgen -G usage without globs.

--- lib/backend_scripts/process-primer-sets.sh
+++ lib/backend_scripts/process-primer-sets.sh
@@ -9,8 +9,8 @@ psql -h "$DB_HOST" -d "$DB_NAME" -U "$DB_USER_RO" -c "SELECT name FROM primer_se
 line_count="$(wc -l < "$new_primers_file")"
 
 # only run the nextflow if there are actually primers, and no other conflicting script is running
-if [ "$line_count" -gt 0 ] && ! compgen -G "$BACKEND_SCRATCH_PATH/status/recomputing_primers.lock" \
-&& ! compgen -G "$BACKEND_SCRATCH_PATH/status/loading_data.lock"; then
+if [ "$line_count" -gt 0 ] && [ ! -e "$BACKEND_SCRATCH_PATH/status/recomputing_primers.lock" ] && \
+[ ! -e "$BACKEND_SCRATCH_PATH/status/loading_data.lock" ]; then
   "$NEXTFLOW_INSTALL_PATH" -log "$BACKEND_SCRATCH_PATH/log_primer_sets/" \
   run "$BACKEND_INSTALL_PATH/lib/pangolin_calls/process_primer_sets.nf" \
   -w "$BACKEND_SCRATCH_PATH/work_primer_sets/" \
