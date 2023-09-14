81964644deaec92ad19415d423673eb6d6abc058
Caiden <ckumar@neb.com>
2023-09-14 15:26:49 -0400

    Removed stray "{"

--- lib/backend_scripts/process-primer-sets.sh
+++ lib/backend_scripts/process-primer-sets.sh
@@ -3,4 +3,4 @@
 SGE_QMASTER_PORT="$HEAD_SGE_QMASTER_PORT" SGE_ROOT="$HEAD_SGE_ROOT" "$QSUB_PATH" -S /bin/bash \
 -v "NXF_JAVA_HOME=$NXF_JAVA_HOME" -v "BACKEND_INSTALL_PATH=$BACKEND_INSTALL_PATH" \
 -o "$BACKEND_SCRATCH_PATH/primers_qsub.log" -e "$BACKEND_SCRATCH_PATH/primers_qsub.err" \
-"$BACKEND_INSTALL_PATH}/lib/backend_scripts/process-primer-sets-run.sh"
+"$BACKEND_INSTALL_PATH/lib/backend_scripts/process-primer-sets-run.sh"
--- lib/backend_scripts/recall-pangolin.sh
+++ lib/backend_scripts/recall-pangolin.sh
@@ -3,4 +3,4 @@
 SGE_QMASTER_PORT="$HEAD_SGE_QMASTER_PORT" SGE_ROOT="$HEAD_SGE_ROOT" "$QSUB_PATH" -S /bin/bash \
 -v "NXF_JAVA_HOME=$NXF_JAVA_HOME" -v "BACKEND_INSTALL_PATH=$BACKEND_INSTALL_PATH" \
 -o "$BACKEND_SCRATCH_PATH/pangolin_qsub.log" -e "$BACKEND_SCRATCH_PATH/pangolin_qsub.err" \
-"$BACKEND_INSTALL_PATH}/lib/backend_scripts/recall-pangolin-run.sh"
+"$BACKEND_INSTALL_PATH/lib/backend_scripts/recall-pangolin-run.sh"
