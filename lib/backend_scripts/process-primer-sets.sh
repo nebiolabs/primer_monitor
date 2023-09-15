08e9f210afb60a3afccb5d79ed5a03cc17b11591
Caiden <ckumar@neb.com>
2023-09-15 16:01:39 -0400

    Fixed pipeline path.

--- lib/backend_scripts/process-primer-sets.sh
+++ lib/backend_scripts/process-primer-sets.sh
@@ -20,9 +20,8 @@ line_count="$(wc -l < "$new_primers_file")"
 
 # only run the nextflow if there are actually primers, and no other conflicting script is running
 if [ "$line_count" -gt 0 ] && [ ! -e "$BACKEND_SCRATCH_PATH/status/loading_data.lock" ]; then
-
   "$NEXTFLOW_INSTALL_PATH" -log "$BACKEND_SCRATCH_PATH/log_primer_sets/" \
-  run "$BACKEND_INSTALL_PATH/lib/pangolin_calls/process_primer_sets.nf" \
+  run "$BACKEND_INSTALL_PATH/lib/process_primer_sets.nf" \
   -w "$BACKEND_SCRATCH_PATH/work_primer_sets/" \
   --primer_monitor_path "$BACKEND_INSTALL_PATH" \
   --output_path "$BACKEND_SCRATCH_PATH" \
