32398abadcbcf78cfa2588d99ced000ecfac1a96
Caiden <ckumar@neb.com>
2023-10-04 11:11:08 -0400

    Added env name suffix to lock file name.

--- config/schedule.rb
+++ config/schedule.rb
@@ -28,19 +28,19 @@ end
 # backend_path is specified through Whenever and does exist when this script is run
 
 every 1.day, at: ['2:00 am'], roles: [:backend] do
-  command "source #{backend_path}/.env && flock \"$LOCK_PATH/primer_monitor_update.lock\" \
+  command "source #{backend_path}/.env && flock \"$LOCK_PATH/primer_monitor_update_$ENV_NAME.lock\" \
   #{backend_path}/lib/backend_scripts/primer-monitor.sh \"#{backend_path}/.env\"", \
           output: "\"$BACKEND_SCRATCH_PATH/download_cron.log\""
 end
 
 every 2.weeks, at: ['3:00 am'], roles: [:backend] do
-  command "source #{backend_path}/.env && flock \"$LOCK_PATH/primer_monitor_update.lock\" \
+  command "source #{backend_path}/.env && flock \"$LOCK_PATH/primer_monitor_update_$ENV_NAME.lock\" \
   #{backend_path}/lib/backend_scripts/recall-pangolin.sh \"#{backend_path}/.env\"", \
           output: "\"$BACKEND_SCRATCH_PATH/pangolin_cron.log\""
 end
 
 every 1.minute, roles: [:backend] do
-  command "source #{backend_path}/.env && flock -n \"$LOCK_PATH/primer_monitor_update.lock\" \
+  command "source #{backend_path}/.env && flock -n \"$LOCK_PATH/primer_monitor_update_$ENV_NAME.lock\" \
   #{backend_path}/lib/backend_scripts/process-primer-sets.sh \"#{backend_path}/.env\"", \
           output: "\"$BACKEND_SCRATCH_PATH/primers_cron.log\""
 end
