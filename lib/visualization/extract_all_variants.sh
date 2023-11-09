161a6f737c1b9715c4c4c2278cbb666bb7addcab
Caiden <ckumar@neb.com>
2023-11-09 15:28:33 -0500

    Fixed reference to organisms.reference_accession.

--- lib/visualization/extract_all_variants.sh
+++ lib/visualization/extract_all_variants.sh
@@ -12,13 +12,13 @@ fi
 
 # WARNING: do not allow arbitrary user data for date cutoff and lineage set, SQL injection risk
 
-psql -h "$DB_HOST" -d "$DB_NAME" -U "$DB_USER_RO" -c "SELECT organisms.reference_accession, variant_sites.ref_start, variant_sites.ref_end, \
+psql -h "$DB_HOST" -d "$DB_NAME" -U "$DB_USER_RO" -c "SELECT organism_taxa.reference_accession, variant_sites.ref_start, variant_sites.ref_end, \
 fasta_records.genbank_accession, lineages.name, fasta_records.date_collected, variant_sites.variant_type, \
 variant_sites.variant FROM variant_sites \
 INNER JOIN fasta_records ON fasta_records.id=variant_sites.fasta_record_id \
 INNER JOIN lineage_calls ON fasta_records.lineage_call_id=lineage_calls.id \
 INNER JOIN lineages ON lineage_calls.lineage_id=lineages.id \
-INNER JOIN organisms ON organisms.id=lineages.organism_id \
+INNER JOIN organism_taxa ON organism_taxa.id=variant_sites.organism_taxon_id \
 WHERE (date_collected >= '$1
 ' OR (date_collected IS NULL AND date_submitted >= '$1')) \
 ORDER BY variant_sites.ref_start, variant_sites.ref_end, variant_sites.variant_type, variant_sites.variant;" --csv -t \
