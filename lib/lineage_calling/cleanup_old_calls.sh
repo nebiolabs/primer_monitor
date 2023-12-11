133edbabfa595b4dd058e6514fe01654b2671789
Caiden <ckumar@neb.com>
2023-12-11 17:59:26 -0500

    Fixed trying to delete lineage calls that are in use.

--- lib/lineage_calling/cleanup_old_calls.sh
+++ lib/lineage_calling/cleanup_old_calls.sh
@@ -3,12 +3,12 @@
 # ensure this is relative to the script's location
 source "$(dirname "$0")/../../.env";
 
-# deletes any pangolin call not connected to a fasta record
+# deletes any lineage call not connected to a fasta record
 
 psql -h "$DB_HOST" -d "$DB_NAME" -U "$DB_USER" >&2 -v "taxon_id=$1" <<CMDS;
-DELETE FROM pangolin_calls WHERE NOT EXISTS
-  (SELECT 1 FROM fasta_records WHERE AND
+DELETE FROM lineage_calls WHERE NOT EXISTS
+  (SELECT 1 FROM fasta_records WHERE
   fasta_records.organism_taxon_id =(SELECT id FROM organism_taxa WHERE organism_taxa.ncbi_taxon_id=:'taxon_id')
-  AND fasta_records.pangolin_call_id=pangolin_calls.id
-  OR fasta_records.pending_pangolin_call_id=pangolin_calls.id);
+  AND fasta_records.lineage_call_id=lineage_calls.id
+  OR fasta_records.pending_lineage_call_id=lineage_calls.id);
 CMDS
--- lib/lineage_calling/swap_calls.sh
+++ lib/lineage_calling/swap_calls.sh
@@ -4,9 +4,6 @@
 source "$(dirname "$0")/../../.env";
 
 psql -h "$DB_HOST" -d "$DB_NAME" -U "$DB_USER" >&2 -v "taxon_id=$1" <<CMDS;
-DELETE FROM lineage_calls WHERE id IN (SELECT lineage_call_id FROM fasta_records
-  WHERE fasta_records.lineage_call_id IS NOT NULL AND fasta_records.pending_lineage_call_id IS NOT NULL AND
-   fasta_records.organism_taxon_id =(SELECT id FROM organism_taxa WHERE organism_taxa.ncbi_taxon_id=:'taxon_id'));
 UPDATE fasta_records SET lineage_call_id=pending_lineage_call_id, pending_lineage_call_id=NULL
   WHERE fasta_records.pending_lineage_call_id IS NOT NULL;
 CMDS
