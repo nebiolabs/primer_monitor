02282e210ecd7c8fb0310d5f7a1761a1ae9c401d
Caiden <ckumar@neb.com>
2024-02-20 12:46:39 -0500

    Only listen on socket outside dev.

--- config/puma.rb
+++ config/puma.rb
@@ -14,12 +14,10 @@ max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
 min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 threads min_threads_count, max_threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port        ENV.fetch('PORT', 3000)
-
 # Specifies the `environment` that Puma will run in.
 if rails_env == 'development'
   workers 1
+  # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
   port ENV.fetch('PORT') { 3000 }
 else
   workers 2
