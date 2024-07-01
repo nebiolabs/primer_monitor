66197a6411b75a50ded2a7033dbc276375a7f0fe
Caiden <ckumar@neb.com>
2024-07-01 14:26:01 -0400

    Added fallback option if default lineage not found.

--- app/javascript/lineage_variants.js
+++ app/javascript/lineage_variants.js
@@ -187,9 +187,23 @@ function initBrowser() {
 
 function setRadiobuttons(selected)
 {
+    let selectionFound = false;
     $('.lineage_set_radiobutton').each(function (index, element) {
         element.checked = (lineageSetsToNames[element.value][0] === selected);
+        if(element.checked) {
+            selectionFound = true
+        }
     });
+    if(!selectionFound) { //if selection missing, select "all" as a fallback
+        let allValue = null;
+        for (let lineageSetKey in lineageSetsToNames) {
+            if (lineageSetsToNames[lineageSetKey][0] === 'all') {
+                allValue = lineageSetKey;
+                break;
+            }
+        }
+        $('.lineage_set_radiobutton[value='+lineageSetsToNames['all']+']')[0].checked = true;
+    }
 }
 
 function setCheckboxes(state)
