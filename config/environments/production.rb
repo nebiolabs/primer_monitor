7c06e673a9f4bcf1d444b42febc0ff66e11074d8
Brad Langhorst <langhorst@neb.com>
2026-05-04 15:01:00 -0400

    we need log rotation - these files were getting huge

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,10 +54,10 @@ Rails.application.configure do
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
-  # Log to STDOUT by default
-  config.logger = ActiveSupport::Logger.new(STDOUT)
-    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
-    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+  config.logger = ActiveSupport::Logger.new(
+    Rails.root.join("log", "#{Rails.env}.log"), 10, 100.megabytes
+  ).tap  { |logger| logger.formatter = ::Logger::Formatter.new }
+   .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
