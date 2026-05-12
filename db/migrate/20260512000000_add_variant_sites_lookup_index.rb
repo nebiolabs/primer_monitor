016983c1c12205ec9df42a54ccc6e44f020a143c
Brad Langhorst <langhorst@neb.com>
2026-05-11 21:13:13 -0400

    Add index on variant_sites for first/last seen lookup query

---
+++ db/migrate/20260512000000_add_variant_sites_lookup_index.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class AddVariantSitesLookupIndex < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
+  def change
+    add_index :variant_sites, %i[ref_start variant_type variant organism_taxon_id],
+              name: 'idx_variant_sites_lookup',
+              algorithm: :concurrently
+  end
+end
